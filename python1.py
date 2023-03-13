2b4ebbaa46399fc280390c2b505e6cc83f4ee62e
Flow-matic <jonathonvinson22@gmail.com>
2023-03-13 19:53:51 +0000

    Added data types, string, integer, float, boolean

--- python1.py
+++ python1.py
@@ -25,3 +25,18 @@ print( 'Hi ' + name , end=' ' )
 print( '- welcome to coding for beginners in easy steps' )
 print( 'remember to have fun ' + name + '!' )
 
+# Data types
+race = 'Daytona 500' # string
+print( race , 'is' , type( race ) )
+
+kilo = 1000  # integer
+print( kilo , 'is' , type( kilo ) )
+
+temp = 98.6  # float
+print( temp , 'is' , type( temp ) )
+
+flag = True  # boolean
+print( flag , 'is' , type( flag ) )
+
+flag = 4 > 8  # boolean
+print( flag , 'is' , type( flag ) )
