6282e4f11786d9190ba9a984876c2a1658b7b0c6
Flow-matic <jonathonvinson22@gmail.com>
2023-04-03 06:55:55 +0000

    Added tuple.py file and created a new class type of days,length, and person

---
+++ python/tuple.py
@@ -0,0 +1,17 @@
+days = ( 'Mon' , 'Tue' , 'Wed' , 'Thur' , 'Fri' , 'Sat' , 'Sun' )
+print( 'days' , type( days ) )
+
+print( 'Days of the week:' , days )
+print( 'No. of days in a week:' , len( days ) )
+print( 'Start day of week:' , days[ 0 ] )
+
+user = ( 'John' , 'Doe' , 'Paris' , '555-1234' )
+print( 'Name:' , user[ 0 ] , user[ 1 ] )
+print( 'Phone:' , user[ 3 ] )
+
+# days <class 'tuple'>
+# Days of the week: ('Mon', 'Tue', 'Wed', 'Thur', 'Fri', 'Sat', 'Sun')
+# No. of days in a week: 7
+# Start day of week: Mon
+# Name: John Doe
+# Phone: 555-1234
