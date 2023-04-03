241b81832e08489a79b1761bdfdee9d04958bb08
Flow-matic <jonathonvinson22@gmail.com>
2023-04-03 08:14:27 +0000

    Created a if.py file to add branching choices code for if, elif, and else statements

---
+++ python/if.py
@@ -0,0 +1,29 @@
+num = int( input( 'Please Enter A Number: ' ) )
+
+if num > 5 :
+    print( 'Number Exceeds 5' )
+elif num < 5 :
+    print( 'Number is Less Than 5' )
+else :
+    print( 'Number Is 5' )
+
+if num > 7 and num < 9 :
+    print( 'Number is 8' )
+
+if num == 1 or num == 3 :
+    print ( 'Number Is 1 or 3' )
+
+# Please Enter A Number: 4
+# Number is Less Than 5
+
+# Please Enter A Number: 6
+# Number Exceeds 5
+
+# Please Enter A Number: 5
+# Number Is 5
+
+# Please Enter A Number: 3
+# Number is Less Than 5
+# Number Is 1 or 3
+
+# The if: elif: else: sequence is the Python equivalent of the switch or case statements found in other languages.
