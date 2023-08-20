a311354458c3db8de0f71b834f772a33388e8423
Flow-matic <jonathonvinson22@gmail.com>
2023-08-20 20:14:30 +0000

    Added output for nest.py code

--- python/nest.py
+++ python/nest.py
@@ -3,3 +3,14 @@ for i in range( 1 , 4 ) :
     for j in range( 1 , 4 ) :
 
         print( 'Running i =' , i , ' j =' , j )
+
+
+# Running i = 1  j = 1
+# Running i = 1  j = 2
+# Running i = 1  j = 3
+# Running i = 2  j = 1
+# Running i = 2  j = 2
+# Running i = 2  j = 3
+# Running i = 3  j = 1
+# Running i = 3  j = 2
+# Running i = 3  j = 3
