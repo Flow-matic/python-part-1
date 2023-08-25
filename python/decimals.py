a353db043e69dba94b49abc0901b705ce0377afd
Flow-matic <jonathonvinson22@gmail.com>
2023-08-25 19:05:41 +0000

    Added time.py file for telling time, This Python code uses the datetime module to work with date and time information. It retrieves the current date and time and then prints various components of the date and time

--- python/decimals.py
+++ python/decimals.py
@@ -14,3 +14,23 @@ print( 'Total:\t',     '%.2f' % total )
 # Item:    0.70
 # Tax:     0.73
 # Total:   1.43
+
+# from decimal import *: Imports the decimal module, which allows you to work with arbitrary-precision decimal numbers.
+
+# item = Decimal(0.70): Creates a Decimal object for the item's price, with a value of 0.70.
+
+# rate = Decimal(1.05): Creates a Decimal object for the tax rate, with a value of 1.05 (105%).
+
+# tax = item * rate: Calculates the tax amount by multiplying the Decimal objects item and rate. The result is a Decimal object with the precise value.
+
+# total = item + tax: Calculates the total cost by adding the item and tax Decimal objects. Again, the result is a Decimal object with precise value.
+
+# print('Item:\t', '%.2f' % item): Prints the item price with two decimal places using the % formatting operator. 
+# This formatting specifies that the value should be displayed with exactly two decimal places.
+
+# print('Tax:\t', '%.2f' % tax): Prints the tax amount with two decimal places using the same % formatting operator.
+
+# print('Total:\t', '%.2f' % total): Prints the total cost with two decimal places using the % formatting operator.
+
+# This code ensures that the values are displayed with two decimal places, and it maintains precision using the decimal module, 
+# which is useful when working with financial calculations or any situation where precision matters.
---
+++ python/time.py
@@ -0,0 +1,57 @@
+from datetime import datetime
+
+today = datetime.now()
+print( 'Today Is:' , today )
+
+for attr in \
+[ 'year' , 'month' , 'day' , 'hour' , 'minute' , 'second' , 'microsecond' ] :
+        print( attr , ':\t' , ':\t' , getattr( today , attr ) )
+
+print( 'Time:' , today.hour , ':' , today.minute , sep = '' )
+
+day = today.strftime( '%A' )
+month = today.strftime( '%B' )
+
+print( 'Date:' , day , month , today.day )
+
+
+# Today Is: 2023-08-25 18:59:18.394778
+# year :   :       2023
+# month :  :       8
+# day :    :       25
+# hour :   :       18
+# minute :         :       59
+# second :         :       18
+# microsecond :    :       394778
+# Time:18:59
+# Date: Friday August 25
+
+
+# from datetime import datetime
+
+# Get the current date and time
+# today = datetime.now()
+# print('Today Is:', today)
+# Here, you import the datetime class from the datetime module and use the datetime.now() function to get the current date and time. 
+# The today variable now holds this information, and it's printed.
+
+# python
+# Loop through and print individual date and time components
+# for attr in ['year', 'month', 'day', 'hour', 'minute', 'second', 'microsecond']:
+#   print(attr, ':\t', ':\t', getattr(today, attr))
+# This loop iterates through a list of strings, where each string represents a date or time component (e.g., 'year', 'month', etc.). 
+# It uses the getattr function to retrieve the corresponding attribute from the today datetime object and prints it. However, 
+# there are two colons (:\t, :\t) after each attribute name that don't seem necessary and might be a typo. The correct code should just have one colon (:).
+
+# Print the time in the format HH:MM
+# print('Time:', today.hour, ':', today.minute, sep='')
+# This code prints the current time in the format "HH:MM" by accessing the hour and minute attributes of the today datetime object and removing any separator (sep='').
+
+# python
+# Format and print the date
+# day = today.strftime('%A')
+# month = today.strftime('%B')
+
+# print('Date:', day, month, today.day)
+# Here, you format and print the date. today.strftime('%A') retrieves the full weekday name, and today.strftime('%B') retrieves the full month name. 
+# today.day is used to get the day of the month. All these components are then printed as "Date: [weekday] [month] [day]".
