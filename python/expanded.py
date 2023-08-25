e8e37891c5ddfbb79b7c71a203cc73ae3d4db61b
Flow-matic <jonathonvinson22@gmail.com>
2023-08-25 18:35:35 +0000

    added expanded.py file for calculating decimals, calculating and printing the item price, tax amount, and total cost with a high precision of 20 decimal places using the .format() method

---
+++ python/expanded.py
@@ -0,0 +1,14 @@
+item = 0.70
+rate = 1.05
+
+tax = item * rate
+total = item + tax
+
+print( 'Item:\t', '{:.20f}'.format( item ) )
+print( 'Tax:\t', '{:.20f}'.format( tax ) )
+print( 'Total:\t', '{:.20f}'.format( total ) )
+
+
+# Item:    0.69999999999999995559
+# Tax:     0.73499999999999998668
+# Total:   1.43500000000000005329
