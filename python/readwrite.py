af837fec8a4bc9943fb93357c2a23f9d37f5b948
Flow-matic <jonathonvinson22@gmail.com>
2023-08-29 18:29:48 +0000

    Added information on what the code does

--- python/readwrite.py
+++ python/readwrite.py
@@ -20,3 +20,18 @@ file.close( )
 file = open( 'poem.txt' , 'a' )
 file.write( '(Oscar Wilde)' )
 file.close( )
+
+
+# I never saw a man who looked
+# With such a wistful eye
+# Upon that little tent of blue
+# Which prisoners call the sky
+
+# Constructs a poem as a string and stores it in the variable poem.
+# Opens a file named 'poem.txt' in write mode ('w') and writes the poem to it.
+# Closes the file.
+# Reopens the 'poem.txt' file in read mode ('r') and prints each line from the file.
+# Closes the file.
+# Reopens the 'poem.txt' file in append mode ('a') and appends the text '(Oscar Wilde)' to it.
+# Closes the file again.
+# This script essentially writes the poem to a text file, reads and prints it, and then appends '(Oscar Wilde)' to the end of the file
